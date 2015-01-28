410aa111d154a6ceffb518761b4cf48efce598b8
Laurent Modolo <laurent@modolo.fr>
2015-01-28 18:08:32 +0100

    modification to get script path in diffTE.R

--- diffTE.R
+++ diffTE.R
@@ -8,10 +8,12 @@
 # biocLite("ggplot2", dep=T)
 # biocLite("RColorBrewer", dep=T)
 
-.libPaths( c( paste0(dirname(sys.frame(1)$ofile),"diffTE"), .libPaths()) )
 
 # --args --FDR_level=0.05 --count_column=3 --count_file=\"count.txt\" --experiment_formula=\"population:extraction\" --sample_names=\"makindu:prot,makindu:size,chicharo:prot,chicharo:size\"
 args = commandArgs(trailingOnly=TRUE)
+scriptPath <- dirname(sub("--file=","",args[grep("--file",args)]))
+.libPaths( c( paste0(scriptPath,"/diffTE"), .libPaths()) )
+
 for(i in 2:length(args))
 {
     eval(parse(text=sub("--", "", args[i])))
@@ -33,7 +35,7 @@ if(exists("version"))
 {
     print("1.0.0")
 }
-
+count_column = count_column-1
 print(FDR_level)
 print(count_column)
 print(count_file)
